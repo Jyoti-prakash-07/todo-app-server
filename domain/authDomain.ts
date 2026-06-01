3d9a3748a84781e5251dc9fe452c1d22b83377b0
Jyoti-prakash-07 <jyotiprakashj810@gmail.com>
2026-06-01 16:25:43 +0530

    new

--- domain/authDomain.ts
+++ domain/authDomain.ts
@@ -1,7 +1,28 @@
-interface RegistrationRequest {
+export interface RegistrationRequest {
     firstName: string;
     lastName: string;
     mobNumber: number;
     email: string;
     password: string;
+}
+export interface UserTableAttributes {
+    id: number;
+    external_id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    mob_no: number;
+    status: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface HashRequest {
+    password: string,
+    salt: number,
 }
